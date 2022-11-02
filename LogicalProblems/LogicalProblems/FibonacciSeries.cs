f5df68ccccb5114322e27733b6a4db336e6086b6
Manoj Kumar Manukonda <manojkumarmanukonda77@gmail.com>
2022-11-02 08:53:51 +0530

    [Manoj] Add: FibonacciSeries

---
+++ LogicalProblems/LogicalProblems/FibonacciSeries.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProblems
+{
+    public class FibonacciSeries
+    {
+        public void Print(int num)
+        {
+            int firstNum = 0, seconfNum = 1, thirdNum = 0;
+            Console.WriteLine(firstNum + "\n" + seconfNum);
+            for (int i = 0; i < num; i++) {
+                thirdNum = firstNum + seconfNum;
+                firstNum = seconfNum;
+                seconfNum = thirdNum;
+                Console.WriteLine(thirdNum);
+            
+            }
+        }
+    }
+}
---
+++ LogicalProblems/LogicalProblems/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace LogicalProblems
+{
+    public class Program
+    {
+
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Welcome to Logical Prolem Solution");
+        }
+    }
+}
