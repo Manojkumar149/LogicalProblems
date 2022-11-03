951db91f8dab08d34c5dfb6d60b2ca48186a74de
Manoj Kumar Manukonda <manojkumarmanukonda77@gmail.com>
2022-11-03 09:35:59 +0530

    [Manoj] Add: CouponNumber

--- LogicalProblems/LogicalProblems/Program.cs
+++ LogicalProblems/LogicalProblems/Program.cs
@@ -5,7 +5,7 @@
 
         static void Main(String[] args)
         {
-            Console.WriteLine("Welcome to Logical Prolem Solution");
+            Console.WriteLine("Welcome to Logical Problem Solution");
 
             //FibonacciSeries series = new FibonacciSeries();
             //series.Print(8);
@@ -16,8 +16,42 @@
             //PrimeNumber primeNumber = new PrimeNumber();
             //primeNumber.CheckPrime(5);
 
-            ReverseNumber reverseNumber = new ReverseNumber();
-            reverseNumber.Reverse(25);
+            //ReverseNumber reverseNumber = new ReverseNumber();
+            //reverseNumber.Reverse(25);
+            //CouponNumber.GenerateCoupons(10);
+            bool flag = true;
+             Console.WriteLine(" 1.FibonacciSeries \n 2.AmstrongNumber \n 3.PrimeNumber \n 4.ReverseNumber \n 5.CouponNumber");
+             int check = Convert.ToInt32(Console.ReadLine());
+             while (flag)
+             {
+                 switch (check)
+                 {
+                     case 1:
+                         FibonacciSeries series = new FibonacciSeries();
+                         series.Print(8);
+                         break;
+                     case 2:
+                         AmstrongNumber amstrongNumber = new AmstrongNumber();
+                         amstrongNumber.Check(5);
+                         break;
+                     case 3:
+                         PrimeNumber primeNumber = new PrimeNumber();
+                         primeNumber.CheckPrime(5);
+                         break;
+                     case 4:
+                         ReverseNumber reverseNumber = new ReverseNumber();
+                         reverseNumber.Reverse(25);
+                         break;
+                    case 5:
+                        CouponNumber.GenerateCoupons(10);
+                        break;
+
+                    default:
+                         break;
+                 }
+                break;
+             }
+           
         }
     }
 }
